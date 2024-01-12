617bbcbc2cf06660635a32e3776f553b32f48663
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2024-01-12 22:56:00 +0100

    Updated API-key

--- XAML_Projektarbete/MainPage.xaml.cs
+++ XAML_Projektarbete/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data.SqlClient;
 using Windows.UI.Xaml.Controls;
 using XAML_Projektarbete.DataProvider;
 
@@ -9,8 +10,16 @@ namespace XAML_Projektarbete
         public MainPage()
         {
             this.InitializeComponent();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=TOMASACERSWIFT5\SQLEXPRESS02;Initial Catalog=Vaccines;Integrated Security=True");
+                connection.Open();
+            }
+            catch { }
+
             APIHelper.InitilizedClient();
-            string apiKey = "42310be47118212874a6";
+            string apiKey = "3105bf0dfaebc4962792";
             CountryDataProvider.ApiKey = apiKey;
             CurrencyDataProvider.ApiKey = apiKey;
             ExchangeRateDataProvider.ApiKey = apiKey;
